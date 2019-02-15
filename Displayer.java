144eed7789a27156bf0d743ec45218ad8f796f24
TEALE-ASUS\teale <tealemasrani@gmail.com>
2019-02-15 03:28:07 -0700

    Refactoring 2: Extracted a class to deal with all displaying

---
+++ Displayer.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class Displayer {
+	
+	private Scanner in;
+	
+	//Constructor
+	public Displayer()
+	{
+		in = new Scanner(System.in);
+	}
+	
+	//Print out the instructions for the whole simulation
+	public void displayInitialInstructions()
+	{
+		System.out.println("\nINSTRUCTIONS: \n\nIn this racing game, there is an SUV in an arctic track and a Sports car in a desert track. \n\nEach turn you can choose if the SUV is to drive in AWD mode \nor normal drive mode, and you can make the Sports car drive too. \n\nEach turn there is a 10% chance of a blizzard hitting the arctic track \nor a heatwave hitting the desert track. \nThese occurances impede the driving of the SUV or the Sports car. \n\nWhichever gets to the end of their track first wins. \nIf both cars run out of fuel before this happens, it is a draw. \nIf you quit early, it is a draw. \n\n");
+	}
+	
+	//Display SUV menu: print options, and read and return user's selection
+	public char displayMenuSUV()
+	{
+		String line;
+		char selection;
+		
+		System.out.println("SUV driving options");
+		System.out.println("(a)ll wheel drive mode");
+		System.out.println("(d)rive normally");
+		System.out.println("(q)uit simulation");
+		System.out.print("Enter selection: ");
+		line = in.nextLine();
+		if (line.length() == 0)
+			line = in.nextLine();
+		
+		selection = line.charAt(0);
+		
+		//Error checking for bad input
+		while (selection != 'd' && selection!= 'D' && selection!= 'c' && selection!= 'C' && selection!= 'q'&& selection!= 'Q')
+		{
+			System.out.println("Please enter 'a', 'd' or 'q'");
+			line = in.nextLine();
+			if (line.length() == 0)
+				line = in.nextLine();
+			
+			selection = line.charAt(0);
+		}
+		selection = Character.toLowerCase(selection);
+		return selection;
+	}
+	
+	//Display Sports car menu: print options, and read and return user's selection
+	public char displayMenuSports()
+	{
+		String line;
+		char selection;
+
+		System.out.println("Sportscar driving options");
+		System.out.println("(d)rive normally");
+		System.out.println("(q)uit simulation");
+		System.out.print("Enter selection: ");
+		line = in.nextLine();
+		if (line.length() == 0)
+			line = in.nextLine();
+		selection = line.charAt(0);
+		
+		//Error checking for bad input
+		while (selection != 'd' && selection!= 'D' && selection!= 'c' && selection!= 'C' && selection!= 'q'&& selection!= 'Q')
+		{
+			System.out.println("Please enter 'a', 'd' or 'q'");
+			line = in.nextLine();
+			if (line.length() == 0)
+				line = in.nextLine();
+			
+			selection = line.charAt(0);
+		}
+		selection = Character.toLowerCase(selection);
+		return selection;
+	}
+	
+	//Display Cheat menu: print options for cheat menu
+	public int displayMenuCheat()
+	{
+		int selection;
+		System.out.println("\nCHEAT MENU SELECTION");
+		System.out.println("(1) Change fuel of sports car");
+		System.out.println("(2) Change fuel of SUV car");
+		System.out.println("(3) Change location of sports car");
+		System.out.println("(4) Change location of suv car");
+		System.out.println("(5) Make a blizzard in the artic track");
+		System.out.println("(6) Make a heat wave in the desert track");
+		System.out.print("Enter selection: ");
+		
+		selection = in.nextInt();
+		
+		while (selection<0 || selection>6)
+		{
+			System.out.println("Please select a number from 1 to 6");
+			selection = in.nextInt();
+		}
+		
+		return selection;
+	}
+	
+	
+}
--- GameController.java
+++ GameController.java
@@ -33,19 +33,14 @@ public class GameController
 	private DesertTrack desTrack;
 	private SUV anSUV;
 	private Sports aSports;
+	private Displayer aDisplayer;
 	
-	private String lineSUV;
-	private String lineSports;
-	private char selectionSUV;
-	private char selectionSports;
-	private int selectionCheat;
-	private int selectionCheatforSports;
-	private int selectionCheatforSUV;
+	
+	private int cheatAffectingSports;
+	private int cheatAffectingSUV;
 	private int newFuelSports;
 	private int newFuelSUV;
-
-	private boolean end = false;
-		
+	
 	
 	//Constructor method for class, instantiates tracks and cars.
 	public GameController()
@@ -56,23 +51,19 @@ public class GameController
         desTrack = new DesertTrack();
 		anSUV = new SUV();
 		aSports = new Sports();
+		aDisplayer = new Displayer();
 	}
-	
-	//Print the instructions for the simulations
-	private void displayInstructions()
-	{
-		System.out.println("\nINSTRUCTIONS: \n\nIn this racing game, there is an SUV in an arctic track and a Sports car in a desert track. \n\nEach turn you can choose if the SUV is to drive in AWD mode \nor normal drive mode, and you can make the Sports car drive too. \n\nEach turn there is a 10% chance of a blizzard hitting the arctic track \nor a heatwave hitting the desert track. \nThese occurances impede the driving of the SUV or the Sports car. \n\nWhichever gets to the end of their track first wins. \nIf both cars run out of fuel before this happens, it is a draw. \nIf you quit early, it is a draw. \n\nBEGIN!\n");
-	}
-	
+
 	// Start: Begins loop for turns, ends loop if one or both cars win, or they both run out of fuel
 	public void start()
 	{
-		displayInstructions();
+		aDisplayer.displayInitialInstructions();
 		arcTrack.display();
 		desTrack.display();
 		
-		while (end == false)
+		while (true)
 		{		
+			System.out.println("NEW ROUND! \n");
 			if (anSUV.isDead == false)
 			{	
 				if (anSUV.getFuel() <=0){
@@ -89,6 +80,8 @@ public class GameController
 				else
 					runTurnSports();
 			}
+			
+			//Check if the game is over
 			if (anSUV.isDead == true && aSports.isDead == true)	
 				bothOutofFuel();
 			if (arcTrack.getLocation() >= 24 && desTrack.getLocation() >= 24)
@@ -98,15 +91,16 @@ public class GameController
 			if (desTrack.getLocation() >= 24)
 				winSports();
 			
-			System.out.println("NEXT ROUND! \n");
 			arcTrack.display();
 			desTrack.display();
 		}
 	}
+	
+	/**************************** BEHAVIOUR FOR WHEN THE GAME ENDS **************************************/
+	
 	//When both cars run out of fuel: print message, display tracks, end program
 	private void bothOutofFuel()
 	{
-		end = true;
 		System.out.println("Both cars are out of fuel, simulation is a draw.");
 		arcTrack.display();
 		desTrack.display();
@@ -140,76 +134,45 @@ public class GameController
 	private void quitEarly()
 	{
 		System.out.println("Quitting game before end: simulation is a draw.");
-		arcTrack.display();
-		desTrack.display();
 		System.exit(0);
 	}
-	//Run the SUV turn: display and process menu, and loop until valid input is entered
+	/******************************************************************************************/
+
+	//Run the SUV turn: display and process menu
 	private void runTurnSUV()
 	{
-		do
-		{
-			displayMenuSUV();
-			processMenuSUV(selectionSUV);
-			System.out.println("");
-		}
-		while (selectionSUV != 'a' && selectionSUV != 'A' && selectionSUV != 'd' && selectionSUV != 'D' && selectionSUV != 'c' && selectionSUV != 'C' && selectionSUV != 'q' && selectionSUV != 'Q');
-	
+		char selection;
+
+		selection = aDisplayer.displayMenuSUV();
+		processMenuSUV(selection);
+		System.out.println("");	
 	}
-	//Run the Sports car turn: display and process menu, and loop until valid input is entered
+	
+	//Run the Sports car turn: display and process menu
 	private void runTurnSports()
 	{
-		do
-		{
-			displayMenuSports();
-			processMenuSports(selectionSports);
-			System.out.println("");
-		}
-		while (selectionSports != 'd' && selectionSports != 'D' && selectionSports != 'c' && selectionSports != 'C' && selectionSports != 'q'&& selectionSports != 'Q');
-	
-	}
-	
-	//Display SUV menu: print options, and read input
-	private void displayMenuSUV()
-	{
-		System.out.println("SUV driving options");
-		System.out.println("(a)ll wheel drive mode");
-		System.out.println("(d)rive normally");
-		System.out.println("(q)uit simulation");
-		System.out.print("Enter selection: ");
-		lineSUV = in.nextLine();
-		if (lineSUV.length() == 0)
-			lineSUV = in.nextLine();
-		selectionSUV = lineSUV.charAt(0);
-	}
-	//Display Sports car menu: print options, and read input
-	private void displayMenuSports()
-	{
-		System.out.println("Sportscar driving options");
-		System.out.println("(d)rive normally");
-		System.out.println("(q)uit simulation");
-		System.out.print("Enter selection: ");
-		lineSports = in.nextLine();
-		if (lineSports.length() == 0)
-			lineSports = in.nextLine();
-		selectionSports = lineSports.charAt(0);
+		char selection;
+		
+		selection = aDisplayer.displayMenuSports();
+		processMenuSports(selection);
+		System.out.println("");
 	}
 	
 	//SUV menu case 'd' or 'a': move SUV according to AWD selection, and if a cheat is being acted on the SUV or not
 	private void SUVcaseAorD(boolean AWD)
 	{
-		if (selectionCheatforSUV != 5)
+		if (cheatAffectingSUV != 5)
 			arcTrack.decideRandBlizzard();
 			
-		if (selectionCheatforSUV == 4){
+		if (cheatAffectingSUV == 4){
 			System.out.println("\nSUV is moving FROM the location set by the cheat. ");
 			arcTrack.move(AWD);
-			selectionCheatforSUV = -1;}
+			cheatAffectingSUV = -1;}
 			
-		else if (selectionCheatforSUV == 2){
+		else if (cheatAffectingSUV == 2){
 			System.out.println("\nSUV fuel has been set to " +newFuelSUV +", due to cheat.");
 			arcTrack.move(AWD, newFuelSUV);
-			selectionCheatforSUV = -1;}	
+			cheatAffectingSUV = -1;}	
 		
 		else {
 			arcTrack.move(AWD);}
@@ -222,18 +185,18 @@ public class GameController
 	//Sports car menu case 'd': move sports car according to if a cheat is being acted on it or not
 	private void SportsCaseD()
 	{
-		if (selectionCheatforSports != 6)
+		if (cheatAffectingSports != 6)
 			desTrack.decideRandHeatwave();
 			
-		if (selectionCheatforSports == 3){
+		if (cheatAffectingSports == 3){
 			System.out.println("\nSports car is moving FROM the location set by the cheat. ");
 			desTrack.move();
-			selectionCheatforSports = -1;}
+			cheatAffectingSports = -1;}
 			
-		else if (selectionCheatforSports == 1){
+		else if (cheatAffectingSports == 1){
 			System.out.println("\nSports car fuel has been set to " +newFuelSports +", due to cheat. ");
 			desTrack.move(newFuelSports);
-			selectionCheatforSports = -1;}
+			cheatAffectingSports = -1;}
 		
 		else{
 			desTrack.move();}
@@ -250,22 +213,18 @@ public class GameController
 	{	
 		switch(selection)
 		{
-			case 'A':
 			case 'a':
 				SUVcaseAorD(true);
 				break;
 	
-			case 'D':
 			case 'd':				
 				SUVcaseAorD(false);
 				break;
 	
-			case 'Q':
 			case 'q':
 				quitEarly();
 				break;
 			
-			case 'C':
 			case 'c':
 				invokeCheatMenu();
 				break;	
@@ -276,57 +235,31 @@ public class GameController
 	}
 	
 	
-	//Process Sports car menu: Carry out actions for whichever Sportcs car option user selected
+	//Process Sports car menu: Carry out actions for whichever Sports car option user selected
 	private void processMenuSports(char selection)
 	{
 		switch(selection)
 		{
-			case 'D':
 			case 'd':	
 				SportsCaseD();		
 				break;
 	
-			case 'Q':
 			case 'q':
 				quitEarly();
 				break;
 			
-			case 'C':
 			case 'c':
 				invokeCheatMenu();
 				break;	
-	
-			default:
-				System.out.println("Please enter 'd' or 'q'");
 		}
 	}
-	
-	
-	
+
 	// When user invokes cheat menu: display and process menu, and loop until valid input is entered
 	private void invokeCheatMenu()
 	{
-		do
-		{
-			displayMenuCheat();
-			selectionCheat = in.nextInt();
-			processMenuCheat(selectionCheat);
-		}
-		while (selectionCheat<0 || selectionCheat>6);
-	}
-	
-	//Display Cheat menu: print options for cheat menu
-	public void displayMenuCheat()
-	{
-		System.out.println("\nCHEAT MENU SELECTION");
-		System.out.println("(0) Toggle debugging messages on/off");
-		System.out.println("(1) Change fuel of sports car");
-		System.out.println("(2) Change fuel of SUV car");
-		System.out.println("(3) Change location of sports car");
-		System.out.println("(4) Change location of suv car");
-		System.out.println("(5) Make a blizzard in the artic track");
-		System.out.println("(6) Make a heat wave in the desert track");
-		System.out.print("Enter selection: ");
+		int selection;
+		selection = aDisplayer.displayMenuCheat();
+		processMenuCheat(selection);
 	}
 	
 	//Cheat menu OPTION 1
@@ -377,45 +310,35 @@ public class GameController
 	{
 		switch(selection)
 		{
-			case 0:
-				if (Debug.on == false)
-					Debug.on = true;
-				else 
-					Debug.on = false;
-				break;
-				
 			case 1:
 				cheatFuelSports();
-				selectionCheatforSports = selection;
+				cheatAffectingSports = selection;
 				break;
 				
 			case 2:
 				cheatFuelSUV();
-				selectionCheatforSUV = selection;
+				cheatAffectingSUV = selection;
 				break;
 				
 			case 3:
 				cheatDesertLocation();
-				selectionCheatforSports = selection;
+				cheatAffectingSports = selection;
 				break;
 				
 			case 4:
 				cheatArcticLocation();
-				selectionCheatforSUV = selection;
+				cheatAffectingSUV = selection;
 				break;
 				
 			case 5:
 				cheatBlizzard();
-				selectionCheatforSUV = selection;
+				cheatAffectingSUV = selection;
 				break;
 				
 			case 6:
 				cheatHeatwave();
-				selectionCheatforSports = selection;
+				cheatAffectingSports = selection;
 				break;
-				
-			default:
-				System.out.println("Please select a number from 0 to 6");
 		}
 	}
 }
